e0c9fcc32fef3a7463a04b129b3673cb073c4ebb
Maksym Zapisov <max.zapisov@gmail.com>
2024-02-01 19:43:05 +0100

    Bug 12056. Updated config.

--- e2e/vitest.config.ts
+++ e2e/vitest.config.ts
@@ -7,21 +7,11 @@ export default defineConfig({
         include: ['./e2e/**/*.test.ts'],
         hookTimeout: 60 * 1000,
         threads: false,
-        server: {
-            deps: {
-                cacheDir: process.cwd() + '/e2e/node_modules/.vite-e2e'
-            }
-        },
         deps: {
             optimizer: {
-                web: {
-                    exclude: [
-                        '@testing-library/preact',
-                        '@testing-library/svelte'
-                    ],
-                },
                 ssr: {
-                    exclude: [
+                    enabled: true,
+                    include: [
                         '@testing-library/preact',
                         '@testing-library/svelte'
                     ],
