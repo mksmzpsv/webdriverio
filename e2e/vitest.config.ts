5bcc4151468f60df06656c078573292b0e99f04a
Maksym Zapisov <max.zapisov@gmail.com>
2024-02-01 21:47:04 +0100

    Restored vitest.config.

--- e2e/vitest.config.ts
+++ e2e/vitest.config.ts
@@ -4,11 +4,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     test: {
         testTimeout: 1000 * 60 * 3,
-        include: ['./e2e/**/*.test.{ts, js, tsx}'],
+        include: ['./e2e/**/*.test.ts'],
         hookTimeout: 60 * 1000,
         threads: false,
-        // testTransformMode: {
-        //     web: ['./e2e/browser-runner/*.test.{ts, js, tsx}']
-        // }
     }
 })
